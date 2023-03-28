33da647acf06941251a9da79c5f23100402b4486
ned <nedschultz@outlook.com>
2023-03-28 15:51:02 +1000

    finished

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,11 @@
 class Anagram
-  attr_accessor :name
-
-  def initialize(word)
-    @name = word
-  end
-
-  def match(array)
-    array.select { |x| x.split("").sort == @name.split("").sort }
-  end
+    attr_accessor :word
+  
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select { |w| w.downcase.chars.sort == @word.downcase.chars.sort && w.downcase != @word.downcase }
+    end
 end
